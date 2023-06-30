84f45bf823509ecfe276eaede544932e470fb845
alexdeathway <alexdeathway@gmail.com>
2023-06-30 21:53:01 +0530

    added templates and created views

--- recipe/models.py
+++ recipe/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.text import slugify
 
 class Recipe(models.Model):
-    #author = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     ingredients = models.TextField()
     servings = models.PositiveIntegerField()
--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,4 @@
+from django import http
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
 from recipe.forms import RecipeCreateForm, RecipeUpdateForm
@@ -12,10 +13,16 @@ class RecipeDetailView(DetailView):
     model = Recipe
     template_name = 'recipe/recipe_detail.html'
 
+
 class RecipeCreatView(CreateView,LoginRequiredMixin):
     model = Recipe
     template_name = 'recipe/recipe_create.html'
     form_class=RecipeCreateForm
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
 
 
 class RecipeUpdateView(UpdateView,LoginRequiredMixin):
@@ -23,7 +30,19 @@ class RecipeUpdateView(UpdateView,LoginRequiredMixin):
     template_name = 'recipe/recipe_update.html'
     form_class=RecipeUpdateForm
 
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().author != self.request.user:
+            raise http.Http403("Privilege Error")
+        return super().dispatch(request, *args, **kwargs)
+
+
 class RecipeDeleteView(DeleteView,LoginRequiredMixin):
     model = Recipe
     template_name = 'recipe/recipe_delete.html'
     success_url = '/recipe/'
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().author != self.request.user:
+            raise http.Http403("Privilege Error")
+        return super().dispatch(request, *args, **kwargs)
+    
